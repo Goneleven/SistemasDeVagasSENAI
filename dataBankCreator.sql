6ee34d8433be81f2139d1db2f601f1ef1936577f
Arthur  Cruz <119413795+ArthurGdSC@users.noreply.github.com>
2023-10-31 07:49:40 -0300

    .

--- dataBankCreator.sql
+++ dataBankCreator.sql
@@ -22,6 +22,7 @@ CREATE TABLE vaga(
 	salario VARCHAR(190),
 	beneficios VARCHAR(190),
 	modalidade VARCHAR(190),
+	emailContato VARCHAR(200)
 );
 
 CREATE TABLE aluno(
@@ -50,9 +51,9 @@ VALUES
 ('Juan', 'Sou o Juan', 'juan@email.com', '12345', '12345678-90', 'Cursando Desenvolvimento de Sistemas no ultimo semestre'),
 ('Igor', 'Sou o Igor', 'igor@email.com', '23456', '23456789-01', 'Cursando Desenvolvimento de Sistemas no ultimo semestre')
 
-INSERT INTO vaga(id_publicador, area, descricao, responsabilidade, jornada, requisitos, localidade, salario, beneficios, modalidade)
+INSERT INTO vaga(id_publicador, area, descricao, responsabilidade, jornada, requisitos, localidade, salario, beneficios, modalidade,emailContato)
 VALUES
-(1, 'TI', 'construir um sistema web de compartilhamento de vagas', 'muita', '18 as 22', 'Ser do técnico de desenvolvimento de sistemas SENAI', 'Mogi das Cruzes', '1000', 'nenhum', 'ead');
+(1, 'TI', 'construir um sistema web de compartilhamento de vagas', 'muita', '18 as 22', 'Ser do técnico de desenvolvimento de sistemas SENAI', 'Mogi das Cruzes', '1000', 'nenhum', 'ead','senai@gmial.com');
 
 
 UPDATE aluno SET curriculum = Cursando Desenvolvimento de Sistemas no ?ltimo semestre,sobreMim_aluno = Sou o Arthur gatãpo, email_aluno = arthur@email.com where id_aluno = 1
@@ -62,6 +63,6 @@ select * from inscricao
 
 SELECT * FROM vaga
 
-drop table aluno
+drop table vaga
 
 
