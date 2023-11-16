ec9ff1120ebc9d9ada593a55302ca8a1552ec60a
Arthur  Cruz <119413795+ArthurGdSC@users.noreply.github.com>
2023-11-16 09:22:56 -0300

    .

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -47,12 +47,92 @@ const getAlVagasInLearner = async () => {
   let res = await fetch(`http://localhost:3000/perfil/vagasEmAluno/${dadosUsuario}`);
   let resJson = await res.json();
   alunos = resJson;
-  console.log(alunos);
+  console.log(vagas);
 
   // testeQoL(alunos);
 
 updateListaAlunos(alunos);  
 
+}
+
+
+function updateListaAlunos(vagas) {
+
+
+  const infoCurriculum2 = document.getElementById("infoCurriculum2");
+  infoCurriculum2.innerHTML = "";
+
+ 
+  for (let i = 0; i < alunos.length; i++) {
+
+    vagas.reverse();
+    let vagaAtual = vagas[i];
+
+    console.log(vagaAtual.area);
+
+
+    const card = document.createElement('div');
+    card.onclick = function () {
+      moreInfo(vagaAtual);
+    };
+    card.classList.add('card', 'border', 'border-dark', 'mb-4', 'cardVaga');
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body', 'popUpMobile');
+
+
+    const titulo_logo = document.createElement('div');
+    titulo_logo.classList.add('d-flex', 'align-items-center');
+
+
+    const titulo = document.createElement('h5');
+    titulo.classList.add('card-title', 'mx-3', 'mr-3');
+    titulo.textContent = vagaAtual.area;
+
+
+    const divisoriaCard = document.createElement('div');
+    divisoriaCard.classList.add('divisoriaCard');
+
+
+    const paragrafo1 = document.createElement('p');
+    paragrafo1.classList.add('card-text', 'my-3');
+    paragrafo1.textContent = vagaAtual.descricao;
+    const descricaoTexto = vagaAtual.descricao;
+
+
+    const paragrafo2 = document.createElement('p');
+    paragrafo2.classList.add('card-text');
+    paragrafo2.innerHTML = '<strong>R$ </strong>' + vagaAtual.salario + '<i class="fa-solid fa-building mx-2"></i>' + vagaAtual.modalidade;
+
+
+    
+    titulo_logo.appendChild(titulo);
+
+    cardBody.appendChild(titulo_logo);
+    cardBody.appendChild(divisoriaCard);
+    cardBody.appendChild(paragrafo1);
+    cardBody.appendChild(paragrafo2);
+
+    if (!logado) {
+      cardBody.appendChild(vagaApagar);
+    }
+
+    card.appendChild(cardBody);
+
+
+
+    container.appendChild(card);
+
+    if (descricaoTexto.length <= 100) {
+      paragrafo1.textContent = descricaoTexto;
+    } else {
+      paragrafo1.textContent = descricaoTexto.substring(0, 100) + '...';
+    }
+
+
+  }
+
+  
 }
 
 
